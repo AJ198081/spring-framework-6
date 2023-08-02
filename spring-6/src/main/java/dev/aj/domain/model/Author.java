9aa973a59c05203556110cb49a19ff2f61162dd4
AJ <amarjitbhandal@gmail.com>
2023-08-02 22:48:50 +1000

    Add Author.java and Book.java

--- spring-6/src/main/java/dev/aj/domain/model/Author.java
+++ spring-6/src/main/java/dev/aj/domain/model/Author.java
@@ -1,4 +1,30 @@
 package dev.aj.domain.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.util.Set;
+
+@Entity(name = "author")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    @JdbcTypeCode(SqlTypes.BIGINT)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books;
 }
--- spring-6/src/main/java/dev/aj/domain/model/Book.java
+++ spring-6/src/main/java/dev/aj/domain/model/Book.java
@@ -1,4 +1,33 @@
 package dev.aj.domain.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.util.Set;
+
+@Entity(name = "book")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    @JdbcTypeCode(SqlTypes.BIGINT)
+    private Long id;
+
+    private String title;
+    private String isbn;
+
+    @ManyToMany
+    @JoinTable(name = "author_book",
+            joinColumns = @JoinColumn(name = "books_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors;
 }
